00cdd192517150baf2a9da0acfda40ca0805befd
aswinkanna18@gmail.com <aswinkanna18@gmail.com>
2025-09-19 07:59:40 +0000

    union stream table

--- models/staging/stg_events.sql
+++ models/staging/stg_events.sql
@@ -1,14 +1,27 @@
--- models/staging/stg_events.sql
-with source as (
-    select *
+with raw as (
+
+    -- Historical GA4 data
+    select
+        event_date,
+        event_timestamp,
+        user_pseudo_id,
+        event_name,
+        traffic_source.source as source,
+        traffic_source.medium as medium
     from `dbtproject-395506.ga4_demo.events_20210131`
+
+    union all
+
+    -- Real-time streaming data
+    select
+        event_date,
+        event_timestamp,
+        user_pseudo_id,
+        event_name,
+        source,
+        medium
+    from `dbtproject-395506.ga4_demo.events_stream`
 )
 
-select
-    event_date,
-    event_timestamp,
-    user_pseudo_id,
-    event_name,
-    traffic_source.source as source,
-    traffic_source.medium as medium
-from source
+select *
+from raw
