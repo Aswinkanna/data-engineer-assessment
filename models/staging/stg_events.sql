1d7a7cfc7d1f1fc4f9dcf974c5bd65cefec4f045
aswinkanna18@gmail.com <aswinkanna18@gmail.com>
2025-09-19 09:50:50 +0000

    coalsce change

--- models/staging/stg_events.sql
+++ models/staging/stg_events.sql
@@ -6,20 +6,20 @@ with raw as (
         event_timestamp,
         user_pseudo_id,
         event_name,
-        traffic_source.source as source,
-        traffic_source.medium as medium
+        coalesce(traffic_source.source, 'direct') as source,
+        coalesce(traffic_source.medium, 'none') as medium
     from `dbtproject-395506.ga4_demo.events_20210131`
 
     union all
 
     -- Real-time streaming data
     select
-        event_date,
-        event_timestamp,
+        cast(event_date as string) as event_date,
+        cast(unix_micros(event_timestamp) as int64) as event_timestamp,
         user_pseudo_id,
         event_name,
-        source,
-        medium
+        'direct' as source,                 
+        'none' as medium
     from `dbtproject-395506.ga4_demo.events_stream`
 )
 
